f36a4af71bbbf6f148fbf16d48231b0a03f31dbf
BDTPierre <buipierre@hotmail.fr>
2024-01-08 14:53:50 +0100

    Updated

--- main.js
+++ main.js
@@ -4,18 +4,26 @@
 // La boucle while sert a demander à l'utilisateur un nombre tant qu'il a pas trouver le nombre juste.
 // Dans cette boucle while on va mettre des conditions: Trop haut, trop bas ou correct et quand il est correct, ca arrete la boucle while et il recoit un message de felicitation.
 
-let Randomnumber = Math.floor(Math.random() * (101 - 0) + 0 );
-// console.log(numberfix)
-let usernumber = window.parseInt(prompt("Trouve le bon nombre"));
-// console.log(usernumber)
+let Randomnumber = Math.floor(Math.random() * (101 - 0) + 0);
+//console.log(numberfix);
+let usernumber = window.parseInt(prompt("Bonjour, vous allez devoir deviner un nombre entre 1 et 100 en 5 tentatives, c'est parti !"));
+//consol.log(usernumber);
+let tentative = 1;
 
-while (usernumber !== Randomnumber){
-    if (usernumber > Randomnumber) {
-        alert ("Vous êtes trop haut!")
+while (usernumber !== Randomnumber && tentative < 5){
+    tentative ++
+    if (usernumber > Randomnumber){
+        alert("C'est moins, retentez votre chance !");
     } else {
-    alert ("Vous êtes trop bas!")
+        alert ("C'est plus, retentez votre chance !");
     }
-    usernumber = window.parseInt(prompt("Trouve le bon nombre"))
+    usernumber = window.parseInt(prompt("Entrez un nombre compris entre 1 et 100 :"));
 }
 
-alert ("Bravo, vous avez trouver le bon nombre!")
+//console.log(essaie);
+if (usernumber === Randomnumber) {
+    alert ("Vous avez le bon nombre. Bravo vous l'avez trouvé en " + tentative + " tentative(s)");
+} if (tentative >= 5) {
+    alert("Vous avez perdu, le nombre qu'il fallait trouver est : " + Randomnumber);
+}
+
